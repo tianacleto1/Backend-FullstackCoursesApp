aa333cece5dfe33b444fb0eba15d700ee56ae42b
Tiago Anacleto <tianacleto1@gmail.com>
2019-07-18 12:33:34 -0300

    Initial project commit

---
+++ src/main/java/com/anacleto/coursesapp/BackendFullstackCoursesApp/BackendFullstackCoursesAppApplication.java
@@ -0,0 +1,13 @@
+package com.anacleto.coursesapp.BackendFullstackCoursesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFullstackCoursesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFullstackCoursesAppApplication.class, args);
+	}
+
+}
